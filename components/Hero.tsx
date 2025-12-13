39ee8671df5122a723804e1b77464c08ded639ce
SeoSangmin <seoinseoulcity@gmail.com>
2025-12-13 15:24:25 +0900

    add: gallery photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,19 @@ import mainPhotoComponent from "@/app/assets/main_photo_components.png";
 export default function Hero() {
     return (
         <section className="relative w-full h-screen overflow-hidden bg-transparent">
+            {/* Title */}
+            <div className="absolute top-[4%] w-full text-center z-20 pointer-events-none">
+                <h1
+                    className="text-[#5A4D4D] tracking-[0.2em] whitespace-nowrap px-4"
+                    style={{
+                        fontFamily: 'AtSign, cursive',
+                        fontSize: 'clamp(1.2rem, 4vw, 3rem)'
+                    }}
+                >
+                    s a v e &nbsp; t h e &nbsp; d a t e
+                </h1>
+            </div>
+
             {/* Content Image */}
             <div className="relative z-10 w-full h-full flex justify-center items-center p-4">
                 <Image
